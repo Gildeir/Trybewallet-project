e45580dce63361a6f33f1ea21cf0eb7b20d762d0
Gildeir <gildeirlopes@gmail.com>
2021-06-08 16:07:36 -0300

    Commit inicial

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
-// import user from './user';
-// import wallet from './wallet';
+import { combineReducers } from 'redux';
+import user from './user';
+import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+export default combineReducers({ user, wallet });
