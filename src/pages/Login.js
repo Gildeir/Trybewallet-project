c733d10561aab53fd07de2f0b45330b72683947b
Gildeir <gildeirlopes@gmail.com>
2021-06-08 19:26:49 -0300

    Adicionando state email e password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { type, value } }) {
+    this.setState(() => ({
+      [type]: value,
+    }));
+  }
+
+  handleClick() {
+    this.setState(() => {
+
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <form>
@@ -10,17 +33,20 @@ class Login extends React.Component {
               data-testid="email-input"
               type="email"
               placeholder="Digite o email"
+              onChange={ this.handleChange }
+              value={ email }
             />
           </label>
           <input
             data-testid="password-input"
             type="password"
             placeholder="Digite a senha"
+            onChange={ this.handleChange }
+            value={ password }
           />
-          <button type="submit">Entrar</button>
+          <button onClick={ this.handleClick } type="submit">Entrar</button>
         </form>
       </div>
-
     );
   }
 }
