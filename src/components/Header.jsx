99e9b22c189091a7beb1bc2b26ccc409851f0a62
Gildeir <gildeirlopes@gmail.com>
2021-06-10 03:18:51 -0300

    Refatorando header

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import login from '../actions';
+import Logo from '../image/Trybe_logo-baixa.png';
+
+class Header extends React.Component {
+  render() {
+    const total = 0;
+    const currency = 'BRL';
+    const { email } = this.props;
+    return (
+      <header>
+        <img src={ Logo } width="150x" alt="trybe_logo" />
+        <span data-testid="email-field">
+          {` Login: ${email}`}
+        </span>
+        <span data-testid="total-field">
+          {` Total: ${total}`}
+        </span>
+        <span data-testid="header-currency-field">
+          {` Currency: ${currency}`}
+        </span>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = ({ user: { email } }) => ({
+  email,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveLogin: (email) => dispatch(login(email)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,36 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import login from '../actions';
-import Logo from '../image/Trybe_logo-baixa.png';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    const { email } = this.props;
     return (
-      <div>
-        <form>
-          <header>
-            <img
-              src={ Logo }
-              width="150x"
-              alt="trybe_logo"
-            />
-            <label htmlFor="email" data-testid="email-field">
-              Email:
-              { email }
-            </label>
-            <label htmlFor="total" data-testid="total-field">
-              Depesa total gerada:
-              <input type="number" value={ 0 } />
-            </label>
-            <label htmlFor="currency" data-testid="header-currency-field">
-              Câmbio:
-              <input type="text" value="BLR" />
-            </label>
-          </header>
-        </form>
-
-      </div>);
+      <Header />);
   }
 }
 
